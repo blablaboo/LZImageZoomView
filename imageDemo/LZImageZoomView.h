7ea9949f17a3d50ea81d276f63579962e9d67ca0
luojianzhen <uroadMac@URoad-MPdeMBP.lan>
2015-05-12 16:16:52 +0800

    first commit

---
+++ imageDemo/LZImageZoomView.h
@@ -0,0 +1,13 @@
+//
+//  LZImageZoomView.h
+//  imageDemo
+//
+//  Created by URoad_MP on 15/5/12.
+//  Copyright (c) 2015年 future. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LZImageZoomView : UIView
+
+@end
---
+++ imageDemo/LZImageZoomView.m
@@ -0,0 +1,21 @@
+//
+//  LZImageZoomView.m
+//  imageDemo
+//
+//  Created by URoad_MP on 15/5/12.
+//  Copyright (c) 2015年 future. All rights reserved.
+//
+
+#import "LZImageZoomView.h"
+
+@implementation LZImageZoomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
