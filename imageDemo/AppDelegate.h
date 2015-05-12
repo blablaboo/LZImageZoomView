9f4f4070f6af350634210f5f3679b8efabb3b77f
luojianzhen <uroadMac@URoad-MPdeMBP.lan>
2015-05-12 15:30:01 +0800

    Initial Commit

---
+++ imageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  imageDemo
+//
+//  Created by URoad_MP on 15/5/12.
+//  Copyright (c) 2015年 future. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ imageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imageDemo
+//
+//  Created by URoad_MP on 15/5/12.
+//  Copyright (c) 2015年 future. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
